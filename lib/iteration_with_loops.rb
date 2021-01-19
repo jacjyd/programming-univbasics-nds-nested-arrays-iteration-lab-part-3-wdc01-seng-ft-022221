4641e4d5e8c10bdeb96aa8e605182cb63eef5f2b
Diana Liao <dijali@gmail.com>
2021-01-19 05:39:49 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,5 +3,13 @@ def join_nested_strings(src)
   # Combine all Strings present in the AoA into a single value and return it
   
   mega_string_array = []
+  row_index = 0 
   
+  while row_index < src.count do
+    element_index = 0 
+    while element_index < src[row_index].count do
+      if src[row_index][element_index].
+      end 
+    end
+  end 
 end
